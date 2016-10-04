d08faef62b0b2b9569c00f9c5fcd5bf16474a6d9
kota65535 <kota65535@gmail.com>
2016-10-04 22:56:35 +0900

    a bit modified test initialization

--- spec/sony_camera_remote_api_spec.rb
+++ spec/sony_camera_remote_api_spec.rb
@@ -4,8 +4,4 @@ describe SonyCameraRemoteApi do
   it "has a version number" do
     expect(SonyCameraRemoteApi::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
